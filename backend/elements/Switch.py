18f54b2ab8c24cc4c8d0769ebae5a6c49f3e2ed4
Nils Ost <home@nijos.de>
2023-01-25 23:21:15 +0100

    refined element Switch

--- backend/elements/Switch.py
+++ backend/elements/Switch.py
@@ -7,17 +7,26 @@ class Switch(ElementBase):
         user=ElementBase.addAttr(default='', notnone=True),
         pw=ElementBase.addAttr(default='', notnone=True),
         purpose=ElementBase.addAttr(type=int, default=1, notnone=True),
-        participant_vlan_id=ElementBase.addAttr(default=None)
+        onboarding_vlan_id=ElementBase.addAttr(default=None)
     )
 
     def validate(self):
         errors = dict()
         if self['purpose'] not in range(3):
             errors['purpose'] = 'needs to be 0, 1 or 2'
-        if self['participant_vlan_id'] is not None:
-            v = docDB.get('VLAN', self['participant_vlan_id'])
-            if v is None:
-                errors['participant_vlan_id'] = f"There is no VLAN with id '{self['participant_vlan_id']}'"
-            elif not v['purpose'] == 2:
-                errors['participant_vlan_id'] = f"VLAN purpose needs to be 2 (onboarding) but is '{v['purpose']}'"
+        if self['purpose'] in range(1, 3):
+            if self['onboarding_vlan_id'] is None:
+                errors['onboarding_vlan_id'] = "can't be None for purposes 1 and 2"
+            else:
+                v = docDB.get('VLAN', self['onboarding_vlan_id'])
+                if v is None:
+                    errors['onboarding_vlan_id'] = f"There is no VLAN with id '{self['onboarding_vlan_id']}'"
+                elif not v['purpose'] == 2:
+                    errors['onboarding_vlan_id'] = f"VLAN purpose needs to be 2 (onboarding) but is '{v['purpose']}'"
+                elif docDB.search_one(self.__class__.__name__, {'onboarding_vlan_id': self['onboarding_vlan_id'], '_id': {'$ne': self['_id']}}) is not None:
+                    errors['onboarding_vlan_id'] = 'This VLAN is allready used on a switch as onboarding_vlan'
         return errors
+
+    def save_pre(self):
+        if self['purpose'] == 0:
+            self['onboarding_vlan_id'] = None
--- backend/testcases/Switch.py
+++ backend/testcases/Switch.py
@@ -10,6 +10,8 @@ class TestSwitch(unittest.TestCase):
 
     def test_range_of_purpose(self):
         self.assertEqual(len(Switch.all()), 0)
+        v = VLAN({'number': 1, 'purpose': 2})
+        v.save()
         # if purpose is lower than 0 it can't be saved
         el1 = Switch({'addr': 'sw1', 'purpose': -1})
         result = el1.save()
@@ -21,7 +23,7 @@ class TestSwitch(unittest.TestCase):
         self.assertNotIn('errors', result)
         self.assertEqual(len(Switch.all()), 1)
         # if purpose is bigger than 2 it can't be saved
-        el2 = Switch({'addr': 'sw2', 'purpose': 3})
+        el2 = Switch({'addr': 'sw2', 'purpose': 3, 'onboarding_vlan_id': v['_id']})
         result = el2.save()
         self.assertIn('purpose', result['errors'])
         self.assertEqual(len(Switch.all()), 1)
@@ -31,57 +33,122 @@ class TestSwitch(unittest.TestCase):
         self.assertNotIn('errors', result)
         self.assertEqual(len(Switch.all()), 2)
 
-    def test_participant_vlan_id_FK_or_None(self):
+    def test_onboarding_vlan_id_FK_or_None(self):
         self.assertEqual(len(Switch.all()), 0)
-        # if participant_vlan_id is None, this is ok
-        el = Switch({'addr': 'sw1', 'participant_vlan_id': None})
+        # if onboarding_vlan_id is None, this is ok
+        el = Switch({'addr': 'sw1', 'onboarding_vlan_id': None, 'purpose': 0})
         result = el.save()
         self.assertNotIn('errors', result)
         self.assertEqual(len(Switch.all()), 1)
-        # if participant_vlan_id is a random string, this is not ok
-        el = Switch({'addr': 'sw2', 'participant_vlan_id': 'somerandomstring'})
+        # if onboarding_vlan_id is a random string, this is not ok
+        el = Switch({'addr': 'sw2', 'onboarding_vlan_id': 'somerandomstring', 'purpose': 1})
         result = el.save()
-        self.assertIn('participant_vlan_id', result['errors'])
+        self.assertIn('onboarding_vlan_id', result['errors'])
         self.assertEqual(len(Switch.all()), 1)
-        # if participant_vlan_id is a valid VLAN id, this is ok
+        # if onboarding_vlan_id is a valid VLAN id, this is ok
         v = VLAN({'number': 1, 'purpose': 2})
         v.save()
-        el = Switch({'addr': 'sw3', 'participant_vlan_id': v['_id']})
+        el = Switch({'addr': 'sw3', 'onboarding_vlan_id': v['_id'], 'purpose': 1})
         result = el.save()
         self.assertNotIn('errors', result)
         self.assertEqual(len(Switch.all()), 2)
 
-    def test_participant_vlan_id_purpose_onboarding(self):
+    def test_onboarding_vlan_id_purpose_onboarding(self):
         # vlan needs to be of purpose 2 (onboarding)
         self.assertEqual(len(Switch.all()), 0)
         # if purpose of vlan is 0, this is not ok
         v = VLAN({'number': 1, 'purpose': 0})
         v.save()
-        el = Switch({'addr': 'sw1', 'participant_vlan_id': v['_id']})
+        el = Switch({'addr': 'sw1', 'onboarding_vlan_id': v['_id']})
         result = el.save()
-        self.assertIn('participant_vlan_id', result['errors'])
+        self.assertIn('onboarding_vlan_id', result['errors'])
         self.assertEqual(len(Switch.all()), 0)
         # if purpose of vlan is 1, this is not ok
         v = VLAN({'number': 2, 'purpose': 1})
         v.save()
-        el = Switch({'addr': 'sw2', 'participant_vlan_id': v['_id']})
+        el = Switch({'addr': 'sw2', 'onboarding_vlan_id': v['_id']})
         result = el.save()
-        self.assertIn('participant_vlan_id', result['errors'])
+        self.assertIn('onboarding_vlan_id', result['errors'])
         self.assertEqual(len(Switch.all()), 0)
         # if purpose of vlan is 3, this is not ok
         v = VLAN({'number': 3, 'purpose': 3})
         v.save()
-        el = Switch({'addr': 'sw3', 'participant_vlan_id': v['_id']})
+        el = Switch({'addr': 'sw3', 'onboarding_vlan_id': v['_id']})
         result = el.save()
-        self.assertIn('participant_vlan_id', result['errors'])
+        self.assertIn('onboarding_vlan_id', result['errors'])
         self.assertEqual(len(Switch.all()), 0)
         # if purpose of vlan is 2, this is finally ok
         v = VLAN({'number': 4, 'purpose': 2})
         v.save()
-        el = Switch({'addr': 'sw4', 'participant_vlan_id': v['_id']})
+        el = Switch({'addr': 'sw4', 'onboarding_vlan_id': v['_id']})
         result = el.save()
         self.assertNotIn('errors', result)
         self.assertEqual(len(Switch.all()), 1)
+
+    def test_onboarding_vlan_id_only_used_on_one_switch(self):
+        # a VLAN is only allowed to be used on one Switch
+        self.assertEqual(len(Switch.all()), 0)
+        v1 = VLAN({'number': 1, 'purpose': 2})
+        v1.save()
+        v2 = VLAN({'number': 2, 'purpose': 2})
+        v2.save()
+        # assign a vlan once is fine
+        el = Switch({'addr': 'sw1', 'onboarding_vlan_id': v1['_id']})
+        result = el.save()
+        self.assertNotIn('errors', result)
+        self.assertEqual(len(Switch.all()), 1)
+        # assign the vlan to another switch is not ok
+        el = Switch({'addr': 'sw2', 'onboarding_vlan_id': v1['_id']})
+        result = el.save()
+        self.assertIn('onboarding_vlan_id', result['errors'])
+        self.assertEqual(len(Switch.all()), 1)
+        # but now changing the vlan to a different one is ok
+        el['onboarding_vlan_id'] = v2['_id']
+        result = el.save()
+        self.assertNotIn('errors', result)
+        self.assertEqual(len(Switch.all()), 2)
+        # saving the same switch agian is also ok
+        result = el.save()
+        self.assertNotIn('errors', result)
+
+    def test_onboarding_vlan_id_required_on_purposes(self):
+        # if switch purpose is 1 or 2 the switch needs onboarding_vlan_id if purpose is 0 onboarding_vlan_id gets set to None
+        self.assertEqual(len(Switch.all()), 0)
+        v1 = VLAN({'number': 1, 'purpose': 2})
+        v1.save()
+        v2 = VLAN({'number': 2, 'purpose': 2})
+        v2.save()
+        # purpose 1 can't be saved without vlan
+        el = Switch({'addr': 'sw1', 'purpose': 1, 'onboarding_vlan_id': None})
+        result = el.save()
+        self.assertIn('onboarding_vlan_id', result['errors'])
+        self.assertEqual(len(Switch.all()), 0)
+        # but purpose 1 with vlan is fine
+        el['onboarding_vlan_id'] = v1['_id']
+        result = el.save()
+        self.assertNotIn('errors', result)
+        self.assertEqual(len(Switch.all()), 1)
+        # purpose 2 can't be saved without vlan
+        el = Switch({'addr': 'sw2', 'purpose': 2, 'onboarding_vlan_id': None})
+        result = el.save()
+        self.assertIn('onboarding_vlan_id', result['errors'])
+        self.assertEqual(len(Switch.all()), 1)
+        # but purpose 2 with vlan is fine
+        el['onboarding_vlan_id'] = v2['_id']
+        result = el.save()
+        self.assertNotIn('errors', result)
+        self.assertEqual(len(Switch.all()), 2)
+        # purpose 0 can be saved without vlan
+        el = Switch({'addr': 'sw3', 'purpose': 0, 'onboarding_vlan_id': None})
+        result = el.save()
+        self.assertNotIn('errors', result)
+        self.assertEqual(len(Switch.all()), 3)
+        # also setting a vlan is deleting it on save if purpose is 0
+        el['onboarding_vlan_id'] = v2['_id']
+        self.assertIsNotNone(el['onboarding_vlan_id'])
+        result = el.save()
+        self.assertNotIn('errors', result)
+        self.assertIsNone(el['onboarding_vlan_id'])
 
 
 setup_module = setUpModule
@@ -91,14 +158,14 @@ teardown_module = tearDownModule
 class TestSwitchApi(ApiTestBase):
     _element = Switch
     _path = 'switch'
-    _patch_valid = {'purpose': 2}
+    _patch_valid = {'addr': 'switch4'}
     _patch_invalid = {'purpose': -1}
 
     def setUp(self):
         docDB.clear()
-        self._setup_el1 = {'addr': 'switch1'}
-        self._setup_el2 = {'addr': 'switch2'}
-        self._post_valid = {'addr': 'switch3'}
+        self._setup_el1 = {'addr': 'switch1', 'purpose': 0}
+        self._setup_el2 = {'addr': 'switch2', 'purpose': 0}
+        self._post_valid = {'addr': 'switch3', 'purpose': 0}
         el = self._element(self._setup_el1)
         self.id1 = el.save().get('created')
         el = self._element(self._setup_el2)
