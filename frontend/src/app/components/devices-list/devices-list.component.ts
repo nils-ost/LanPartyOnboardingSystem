8d916bc3088c35be47f3203fd71bfa1d7005d1d2
Nils Ost <home@nijos.de>
2023-09-25 12:44:32 +0200

    reworked all backend calls required for devices-list

--- frontend/src/app/components/devices-list/devices-list.component.ts
+++ frontend/src/app/components/devices-list/devices-list.component.ts
@@ -1,6 +1,12 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges, ViewChild } from '@angular/core';
 import { Device } from 'src/app/interfaces/device';
+import { IpPool } from 'src/app/interfaces/ip-pool';
+import { Participant } from 'src/app/interfaces/participant';
+import { Port } from 'src/app/interfaces/port';
+import { Seat } from 'src/app/interfaces/seat';
+import { Switch } from 'src/app/interfaces/switch';
+import { Table } from 'src/app/interfaces/table';
 import { DeviceService } from 'src/app/services/device.service';
 import { ErrorHandlerService } from 'src/app/services/error-handler.service';
 import { UtilsService } from 'src/app/services/utils.service';
@@ -10,11 +16,22 @@ import { UtilsService } from 'src/app/services/utils.service';
   templateUrl: './devices-list.component.html',
   styleUrls: ['./devices-list.component.scss']
 })
-export class DevicesListComponent {
+export class DevicesListComponent implements OnChanges {
   @Input() devices: Device[] = [];
+  @Input() tables: Table[] = [];
+  @Input() seats: Seat[] = [];
+  @Input() ippools: IpPool[] = [];
+  @Input() participants: Participant[] = [];
+  @Input() ports: Port[] = [];
+  @Input() switches: Switch[] = [];
   @Output() editedDeviceEvent = new EventEmitter<null>();
 
   @ViewChild('editdesc') editDescDialog: any;
+
+  seatsReadable: Map<string, string> = new Map<string, string>;
+  ippoolsReadable: Map<string, string> = new Map<string, string>;
+  participantsReadable: Map<string, string> = new Map<string, string>;
+  portsReadable: Map<string, string> = new Map<string, string>;
 
   selectedDevice: Device | undefined = undefined;
   newDesc: string = "";
@@ -24,6 +41,44 @@ export class DevicesListComponent {
     private errorHandler: ErrorHandlerService,
     private deviceService: DeviceService
   ) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (('tables' in changes || 'seats' in changes) && this.tables.length > 0 && this.seats.length > 0) {
+      let tablesById: Map<string, Table> = new Map<string, Table>;
+      for (let i = 0; i < this.tables.length; i++) tablesById.set(this.tables[i].id, this.tables[i]);
+      for (let i = 0; i < this.seats.length; i++) {
+        let seat: Seat = this.seats[i];
+        let table: Table | undefined = tablesById.get(seat.table_id);
+        if (table) this.seatsReadable.set(seat.id, table.number + '(' + table.desc + ')' + ': #' + seat.number);
+      }
+    }
+
+    if ('ippools' in changes) {
+      for (let i = 0; i < this.ippools.length; i++) {
+        let ippool: IpPool = this.ippools[i];
+        this.ippoolsReadable.set(ippool.id, ippool.desc);
+      }
+    }
+
+    if ('participants' in changes) {
+      for (let i = 0; i < this.participants.length; i++) {
+        let participant: Participant = this.participants[i];
+        let login: string = "";
+        if (participant.login) login = participant.login;
+        this.participantsReadable.set(participant.id, participant.name + ' (' + login + ')');
+      }
+    }
+
+    if (('switches' in changes || 'ports' in changes) && this.switches.length > 0 && this.ports.length > 0) {
+      let switchesById: Map<string, Switch> = new Map<string, Switch>;
+      for (let i = 0; i < this.switches.length; i++) switchesById.set(this.switches[i].id, this.switches[i]);
+      for (let i = 0; i < this.ports.length; i++) {
+        let port: Port = this.ports[i];
+        let swi: Switch | undefined = switchesById.get(port.switch_id);
+        if (swi) this.portsReadable.set(port.id, swi.desc + ': #' + port.number);
+      }
+    }
+  }
 
   editDescStart(device: Device, event: any) {
     this.selectedDevice = device;
