9bdb67d8073121e2ae4bdfee5399a9ff7b6ce1b4
Nils Ost <home@nijos.de>
2024-06-26 23:03:00 +0200

    startet on the PortConifgCache generate function

--- backend/elements/PortConfigCache.py
+++ backend/elements/PortConfigCache.py
@@ -28,4 +28,23 @@ class PortConfigCache(ElementBase):
         docDB.delete_many(cls.__name__, {'port_id': port_id})
 
     def _generate(self):
-        pass
+        if (self['scope'] == 0 and self.port()['commit_disabled']) or (self['scope'] == 1 and self.port()['retreat_disabled']):
+            self['skip'] = True
+            self.save()
+            return
+        else:
+            self['skip'] = False
+
+        # TODO: consider on this stage, that it might be a switchlink port
+        manual_config = self.port()['commit_config'] if self['scope'] == 0 else self.port()['retreat_config']
+        if manual_config is not None:
+            self['vlan_ids'] = manual_config['vlans']
+            self['default_vlan_id'] = manual_config['default']
+            self['enabled'] = manual_config['enabled']
+            self['mode'] = manual_config['mode']
+            self['receive'] = manual_config['receive']
+            self['force'] = manual_config['force']
+            self.save()
+            return
+
+        # TODO: automatic decision making
